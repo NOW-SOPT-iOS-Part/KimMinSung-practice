0095312815c01caa4817878cf55c480e9b382f1a
Minsung Kim <mskim4048@naver.com>
2024-04-19 14:46:21 +0900

    Initial Commit

---
+++ 34th-SOPT-iOS-DanggeunCloneCoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  34th-SOPT-iOS-DanggeunCloneCoding
+//
+//  Created by 김민성 on 2024/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
